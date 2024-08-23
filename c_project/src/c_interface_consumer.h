4f3f9ee676c980ae410b2ec08bbafa103cd50b11
river-martin <rivermartin128@gmail.com>
2024-08-23 10:29:54 +0200

    Update comment

--- c_project/src/c_interface_consumer.h
+++ c_project/src/c_interface_consumer.h
@@ -2,7 +2,7 @@
 #define SUM_NUMBERS_H
 #include "c_number_interface.h"
 
-// This is called from the py_sum_numbers.py
+// This is called from the py_c_num_inteface_shim.c
 C_NumInterface *
 c_sum_numbers(C_NumInterface *a, C_NumInterface *b, C_NumInterface *c);
 #endif // SUM_NUMBERS_H
